b374d11004d2b349eade004c2177bb0e425122a0
Prajwal <prajwalmjain5914@gmail.com>
2024-01-03 20:34:22 +0530

    newbotfeature

--- music.py
+++ music.py
@@ -1,6 +1,20 @@
 import discord
 from discord.ext import commands
+import os, asyncio
 
+from help_cog import help_cog
 from music_cog import music_cog
 
-bot1 = commands.Bot(command_prefix="/")
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='hv', intents=intents)
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+TOKEN  = 'MTE3MzY2Mjg0MTI5MTY3MzY2Mg.Gx6S4Q.mAW_qXTvV0ZWWU76fFs33h4Bn_V9rnPGixjyZk'
+async def main():
+    async with bot:
+        await bot.add_cog(help_cog(bot))
+        await bot.add_cog(music_cog(bot))
+        await bot.start(TOKEN)
+
+asyncio.run(main())
